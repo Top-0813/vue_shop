83f65013432252a22c7f94b709d2aa0376166ddd
文艺小码农 <854261062@qq.com>
2020-04-24 17:50:39 +0800

    完成了商品功能

--- src/main.js
+++ src/main.js
@@ -26,6 +26,20 @@ Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
 // 将富文本注册为全局可用的组件
 Vue.use(VueQuillEditor)
+// 时间过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
